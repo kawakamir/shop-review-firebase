0d13dca9e795c6e4e14f1ab13ad509e5250debd3
kawakamir <rui.kawakami@gmail.com>
2020-10-04 22:41:14 +0900

    update add ave score

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,3 +28,72 @@ exports.onUpdateUser = functions
         }
     })
 
+exports.onWriteReview = functions
+    .region("asia-northeast1")
+    .firestore.document("shops/{shopId}/reviews/{reviewId}")
+    .onWrite(async (change, context) => {
+        const { shopId, reviewId } = context.params;
+        const review = change.after.data() as Review;
+        const db = admin.firestore()
+        try {
+            const shopRef = db.collection("shops").doc(shopId)
+            const shopDoc = await shopRef.get();
+            const shop = shopDoc.data() as Shop;
+
+            let { score1 = 0, score2 = 0, score3 = 0, score4 = 0, score5 = 0 } = shop;
+            if (review.score === 1) {
+                score1 += 1;
+            } else if (review.score === 2) {
+                score2 += 1;
+            } else if (review.score === 3) {
+                score3 += 1;
+            } else if (review.score === 4) {
+                score4 += 1;
+            } else if (review.score === 5) {
+                score5 += 1;
+            } 
+            let aveScore = 
+                (score1 + score2 * 2 + score3 * 3 + score4 * 4 + score5 * 5) /
+                (score1 + score2 + score3 + score4 + score5);
+            aveScore = Math.round(aveScore * 100) / 100;
+
+            let params = {};
+            if (review.score === 1) {
+              params = {
+                score1: admin.firestore.FieldValue.increment(1),
+                score: aveScore,
+              };
+            } else if (review.score === 2) {
+              params = {
+                score2: admin.firestore.FieldValue.increment(1),
+                score: aveScore,
+              };
+            } else if (review.score === 3) {
+              params = {
+                score3: admin.firestore.FieldValue.increment(1),
+                score: aveScore,
+              };
+            } else if (review.score === 3) {
+              params = {
+                score3: admin.firestore.FieldValue.increment(1),
+                score: aveScore,
+              };
+            } else if (review.score === 4) {
+              params = {
+                score4: admin.firestore.FieldValue.increment(1),
+                score: aveScore,
+              };
+            } else if (review.score === 5) {
+              params = {
+                score5: admin.firestore.FieldValue.increment(1),
+                score: aveScore,
+              };
+            }
+            await shopRef.update(params);
+      
+          } catch (err) {
+            console.log(err);
+          }
+        }
+    })
+
